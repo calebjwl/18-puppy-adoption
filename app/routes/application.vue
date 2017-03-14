da626542fdf1be61f023e08de08506b44d4e86f7
Caleb Lee <caleb.gwl@gmail.com>
2017-03-14 14:56:04 -0500

    Load data from store

--- app/routes/application.vue
+++ app/routes/application.vue
@@ -42,11 +42,19 @@
 </template>
 
 <script>
+import store from '../store';
+import { findAll } from '../actions/puppy';
+
 export default {
   data() {
     return {
+      puppies: this.$select('puppies'),
     };
   },
+
+  mounted() {
+    store.dispatch(findAll(this.$route.params.id));
+  }
 
   methods: {
 
